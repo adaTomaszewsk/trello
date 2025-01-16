1bbf1d481f8e6920f2dca9a0eb1435aca88d7fad
ada <adaa.tomaszewska@gmail.com>
2025-01-16 22:59:22 +0100

    create new project

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -7,13 +7,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Project;
+use App\Entity\ProjectColumn;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class ProjectController extends AbstractController {
     private EntityManagerInterface $entityManager;
+    private UserInterface $currentUser;
 
-    public function __construct(EntityManagerInterface $entityManager){
+    public function __construct(EntityManagerInterface $entityManager,  Security $security){
         $this->entityManager = $entityManager;
+        $this->currentUser = $security->getUser();
     }
 
 
@@ -24,6 +30,49 @@ class ProjectController extends AbstractController {
  
         return $this->render('components/sidebar_projects_render.html.twig',[
             'projects' => $projects
+        ]);
+    }
+
+
+    #[Route('api/create-project', name: 'new-project')]
+    public function createProject(Request $request, EntityManagerInterface $entityManager, UserInterface $currentUser): JsonResponse 
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if(!$data || !isset($data['projectName']) || empty(trim($data['projectName']))) {
+            return new JsonResponse(['error' => 'Project name is required'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        $project = new Project();
+        $project->setName($data['projectName']);
+        $project->setCreatedBy($currentUser);
+        $project->setCreatedAt(new \DateTime());
+        $entityManager->persist($project);
+
+        foreach ($data['columns'] as $columnName) {
+            if (empty(trim($columnName))) {
+                return new JsonResponse(['error' => 'Column name cannot be empty.'], JsonResponse::HTTP_BAD_REQUEST);
+            }
+            $column = new ProjectColumn();
+            $column->setName($columnName);
+            $column->setProject($project);
+    
+            $entityManager->persist($column);
+        }
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Project created successfully', JsonResponse::HTTP_CREATED]);
+
+    }
+
+    #[Route('project/{id}', name: 'project_id')]
+    public function projectPage(int $id, EntityManagerInterface $entityManager): Response {
+        $project =  $this->entityManager->getRepository(Project::class)->findOneBy([
+            'id'=> $id
+        ]);
+
+        return $this->render('project/index.html.twig', [
+            'project' => $project,
         ]);
     }
 
