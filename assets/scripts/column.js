bc77de5bbc1f8e8ded6565798eb60064509cce06
ada <adaa.tomaszewska@gmail.com>
2025-01-20 22:52:00 +0100

    add project columns

---
+++ assets/scripts/column.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    $button = document.getElementById('button_add_new_column');
+    
+    $button.addEventListener('click', function () {
+        console.log('aaaaaaaa');
+    });
+});
--- src/Controller/ProjectColumnController.php
+++ src/Controller/ProjectColumnController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Entity\ProjectColumn;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProjectColumnController extends AbstractController {
     private EntityManagerInterface $entityManager;
@@ -12,6 +17,30 @@ class ProjectColumnController extends AbstractController {
         $this->entityManager = $entityManager;
     }
 
-    
+    #[Route('/columns/{id}', name:'all_columns')]
+    public function all_columns(int $id, EntityManagerInterface $entityManager): Response 
+    {
+        $project = $this->entityManager->getRepository(Project::class)->find($id);
 
-}
+        $columns = $entityManager->getRepository(ProjectColumn::class)->findBy(array('project' => $project));
+        return $this->render('columns/columns.html.twig', [
+            'columns' => $columns,
+        ]);
+    }
+
+    #[Route('/add_column', name:'add_column')]
+    public function addColumn(int $id, string $columnName, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $project = $this->entityManager->getRepository(Project::class)->find($id);
+        $column = new ProjectColumn();
+        $column->setName($columnName);
+        $column->setProject($project);
+    
+        $entityManager->persist($column);
+        $entityManager->flush();
+        
+        return new JsonResponse(['message' => 'Project column created successfully', JsonResponse::HTTP_CREATED]);
+
+    }
+
+} 
