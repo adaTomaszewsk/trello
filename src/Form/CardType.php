08865c7b7c0e56116ca0446637be5a18274cd90c
ada <adaa.tomaszewska@gmail.com>
2025-01-28 13:04:36 +0100

    add styles to card modal

--- src/Form/CardType.php
+++ src/Form/CardType.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -21,7 +22,7 @@ class CardType extends AbstractType
                     'placeholder' => 'Enter a title or paste a link'
                 ]
             ])
-            ->add('description', TextType::class, [
+            ->add('description', TextareaType::class, [
                 'attr' => [
                     'placeholder' => 'Enter a description'
                 ]
